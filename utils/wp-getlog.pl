0a41b8b71e31894a4202c8a222671d147ad27b63
lifeich1 <lifeich0@gmail.com>
2023-09-09 23:30:30 +0800

    :bug: fix sec filter

--- utils/wp-getlog.pl
+++ utils/wp-getlog.pl
@@ -6,30 +6,35 @@ use open qw/:std :utf8/;
 use Time::Piece;
 use Time::Seconds;
 
-my $sec_name = (shift or 'Music');
+my $sec_name = ( shift or 'Music' );
 say STDERR "filtering $sec_name";
-my $sec = 'uninit';
+my $sec  = 'uninit';
 my $task = 'uninit';
-my $now = localtime;
+my $now  = localtime;
 
-open(my $fin, '<', '.record.org') or die "open record.org error: $!";
+open( my $fin, '<', '.record.org' ) or die "open record.org error: $!";
 while (<$fin>) {
     chomp;
     if (/\A(\*\*\*?) (?:(\w+)|(?:STUDYING (.+)))\Z/) {
-        if ($1 eq '**') {
+        if ( $1 eq '**' ) {
             $sec = $2;
             say STDERR "cap sec=$sec";
-        } else {
+        }
+        else {
             $task = $3;
             $task =~ s/\[\[\S+\]\[(.+)\]\]/$1/;
             say STDERR "cap task=$task";
         }
-    } elsif (/\A\s+- State "DONE"\s+from "STUDYING"\s+\[(\d+-\d+-\d+)\s\S\s(\d+:\d+)\]\Z/) {
-        my $tag = "$1 $2";
-        my $tag_t = Time::Piece->strptime($tag, "%Y-%m-%d %H:%M");
-        if ($now - $tag_t < ONE_WEEK) {
+    }
+    elsif (
+/\A\s+- State "DONE"\s+from "STUDYING"\s+\[(\d+-\d+-\d+)\s\S\s(\d+:\d+)\]\Z/
+      )
+    {
+        my $tag   = "$1 $2";
+        my $tag_t = Time::Piece->strptime( $tag, "%Y-%m-%d %H:%M" );
+        if ( $now - $tag_t < ONE_WEEK ) {
             say STDERR "cap this week tag=$tag";
-            say "[$tag] $task";
+            say "[$tag] $task" if $sec eq $sec_name;
         }
     }
 }
